cbf5a07581c9dff0fc8cac406fb6d1a214286e9f
xRaerasx <54640189+xRaerasx@users.noreply.github.com>
2020-06-15 22:01:24 -0400

    Update custom_char_fields.rb

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -31,7 +31,7 @@ module AresMUSH
       # Note Custom fields will be in chargen_data[:custom]
       # Example: char.update(goals: chargen_data[:custom][:goals])
       def self.save_fields_from_chargen(char, chargen_data)
-	    char.update(Website.format_input_for_mush(aspirations: chargen_data[:custom][:aspirations]))
+	    char.update(aspirations: chargen_data[:custom][:aspirations])
 		return []
 	  end
       
